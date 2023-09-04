121d6d22f19c623be698f461d246a0c9240ccf31
冀金梁 <625103471@qq.com>
2023-09-04 17:49:17 +0800

    添加错过策略的注解

--- src/main/java/com/example/demo/quartz/job/TestJob.java
+++ src/main/java/com/example/demo/quartz/job/TestJob.java
@@ -19,20 +19,24 @@ public class TestJob {
         JobDetail jobDetail = JobBuilder
                 .newJob(Myjob.class)
                 .withIdentity("job1", "job_group1")   //把任务逻辑的.class文件传入
-                .usingJobData("job_order","625103471")  //设置任务的id，可根据id来防止重复执行，逻辑里面都可以获取到
-                .usingJobData("count",0)     //计数器，可以用来统计任务执行了几次
+                .usingJobData("job_order", "625103471")  //设置任务的id，可根据id来防止重复执行，逻辑里面都可以获取到
+                .usingJobData("count", 0)     //计数器，可以用来统计任务执行了几次
+                .storeDurably() //持久化
                 .build();
 
         //创建触发类
         Trigger trigger = TriggerBuilder
                 .newTrigger()
                 .withIdentity("trigger1", "trigger_group1")
-                .usingJobData("trigger_order","625103471")
+                .usingJobData("trigger_order", "625103471")
                 .startNow()
                 //触发器的类型，有简单、日历、cron等，这里用的是简单
 //                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(1).repeatForever())
                 //cron表达式触发，这里是每5秒执行一次
-                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))
+                //withMisfireHandlingInstructionFireAndProceed  //立即执行错过的第一个任务，忽略其他错过的任务，再按照原计划进行
+                //withMisfireHandlingInstructionDoNothing  错过了什么都不做,按照原计划进行
+                //withMisfireHandlingInstructionIgnoreMisfires  立即去执行错过的任务
+                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?").withMisfireHandlingInstructionIgnoreMisfires())
                 .build();
 
         //创建定时器类
