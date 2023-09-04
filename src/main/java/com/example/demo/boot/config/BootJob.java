e1ca935f3dd619dfaaa1a40f428ffd914cda51bc
冀金梁 <625103471@qq.com>
2023-09-04 11:46:39 +0800

    创建任务

---
+++ src/main/java/com/example/demo/boot/config/BootJob.java
@@ -0,0 +1,35 @@
+package com.example.demo.boot.config;
+
+import org.quartz.*;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@DisallowConcurrentExecution
+@PersistJobDataAfterExecution
+public class BootJob extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        //执行的逻辑
+
+        System.out.println("开始执行任务的时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        JobDataMap triggerMqp = context.getTrigger().getJobDataMap();
+        JobDataMap mergedJobDataMap = context.getMergedJobDataMap(); //可以获取到所有的JobDataMap键值对
+
+        JobDataMap jobDataMap2 = context.getJobDetail().getJobDataMap();
+        jobDataMap2.put("count",jobDataMap2.getInt("count")+1);
+        System.out.println(jobDataMap2.get("count"));
+
+        //如果该任务执行发生了异常，任务再次启动时候，会再次执行该任务，此时这个值为true，可以用户来判断是否重复执行
+        boolean recovering = context.isRecovering();
+        System.out.println(recovering);
+
+        try {
+            Thread.sleep(3000); //任务执行超时也不用担心重复执行
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
